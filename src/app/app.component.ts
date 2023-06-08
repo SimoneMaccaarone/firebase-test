2eb639c151938aa797c3626f8b1b352fba4d1d3a
Simone Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-06-08 11:27:02 +0200

    step 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firebase-test';
+
+  // Your web app's Firebase configuration
+  firebaseConfig = {
+    apiKey: "AIzaSyAIx1Svy_svKsqzfx_ms1XhGtx2nSfHwb0",
+    authDomain: "my-first-project-bad19.firebaseapp.com",
+    projectId: "my-first-project-bad19",
+    storageBucket: "my-first-project-bad19.appspot.com",
+    messagingSenderId: "314231743415",
+    appId: "1:314231743415:web:750035266a1a4ade94be32"
+  };
+  // Initialize Firebase
+  app = initializeApp(this.firebaseConfig);
+  db = getFirestore(this.app);
+
+
+
+
 }
